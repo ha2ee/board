3f0feddcf31c3e61b262d012d5b208ffeb83c0c7
KimDongHyun <donghyun.develop@gmail.com>
2023-07-14 21:21:24 +0900

    refactor: Modify application name in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ec2-user/cicdproject
 cd $REPOSITORY
 
-APP_NAME=cicdproject
+APP_NAME=board
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
