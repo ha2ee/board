8b8545fa9f83c75cd9510ef9ff1a00d412168ee6
KimDongHyun <donghyun.develop@gmail.com>
2023-07-15 19:00:05 +0900

    refactor: application.properties -> application.yml

--- src/main/java/hello/board/config/DatabaseConfig.java
+++ src/main/java/hello/board/config/DatabaseConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.PropertySource;
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:/application.properties")
+@PropertySource("classpath:/application.yml")
 public class DatabaseConfig {
 
     @Autowired
