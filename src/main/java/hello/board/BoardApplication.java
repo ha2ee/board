539383d62ba13b5264f81c48b61ac3912eff418f
KimDongHyun <donghyun.develop@gmail.com>
2023-07-16 12:35:21 +0900

    feat: nginx config

--- src/main/java/hello/board/BoardApplication.java
+++ src/main/java/hello/board/BoardApplication.java
@@ -2,12 +2,20 @@ package hello.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BoardApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/cicd/real-application.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
+//		SpringApplication.run(BoardApplication.class, args);
+		new SpringApplicationBuilder(BoardApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
